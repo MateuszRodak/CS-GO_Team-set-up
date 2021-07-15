ebec44d3ba48d2c75799de2bbb817269f4166561
Mateusz Rodak <mateusz.rodak108@gmail.com>
2021-07-15 20:44:15 +0200

    add two buttons

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function de_dust2()
 {
-    document.getElementById("map").innerText = "red";
+    document.getElementById("de_mirage").style.display = "none"
+    document.getElementById("de_dust2").style.display = "block"
+}
+
+function de_mirage()
+{
+    document.getElementById("de_mirage").style.display = "block"
+    document.getElementById("de_dust2").style.display = "none"
 }
