8bbe3bf4e76a970d22d850977c5563256d3f3aee
Mateusz Rodak <mateusz.rodak108@gmail.com>
2021-07-16 11:55:33 +0200

    addd de train

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function de_dust2()
 {
     document.getElementById("de_mirage").style.display = "none"
+    document.getElementById("de_train").style.display = "none"
     document.getElementById("de_dust2").style.display = "block"
 }
 
 function de_mirage()
 {
     document.getElementById("de_mirage").style.display = "block"
+    document.getElementById("de_train").style.display = "none"
     document.getElementById("de_dust2").style.display = "none"
+}
+
+function de_train()
+{
+    document.getElementById("de_mirage").style.display = "none"
+    document.getElementById("de_dust2").style.display = "none"
+    document.getElementById("de_train").style.display = "block"
 }
 
 function set_up()
