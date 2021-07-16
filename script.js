764f991741a82d1a1bfd79a1ba6730fd1d08dd14
Mateusz Rodak <mateusz.rodak108@gmail.com>
2021-07-16 16:41:03 +0200

    colors working on every map

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+var actualMap = "de_mirage"
+
 function de_dust2()
 {
     document.getElementById("de_mirage").style.display = "none"
     document.getElementById("de_train").style.display = "none"
     document.getElementById("de_dust2").style.display = "block"
+    actualMap = "de_dust2"
 }
 
 function de_mirage()
@@ -10,6 +13,7 @@ function de_mirage()
     document.getElementById("de_mirage").style.display = "block"
     document.getElementById("de_train").style.display = "none"
     document.getElementById("de_dust2").style.display = "none"
+    actualMap = "de_mirage"
 }
 
 function de_train()
@@ -17,25 +21,40 @@ function de_train()
     document.getElementById("de_mirage").style.display = "none"
     document.getElementById("de_dust2").style.display = "none"
     document.getElementById("de_train").style.display = "block"
+    actualMap = "de_train"
 }
 
 function set_up()
 { 
-    document.getElementsByClassName('mirage')[0].style.display = "none"
-    for(var i=0;i<5;i++){
-        var a=Math.floor(Math.random() * 5)+1;
-    }
-    document.getElementById('test').innerHTML=a;
-    changeColor('mirage',0,1,2,3,4)
+    var maplen = document.getElementsByClassName(actualMap);
+
+   // for(var i=0;i<5;i++){
+  //      var a=Math.floor(Math.random() * 5)+1;
+ //   }
+  //  document.getElementById('test').innerHTML=a;
+
+    var a=Math.floor(Math.random() * maplen.length);
+    var b=Math.floor(Math.random() * maplen.length);
+    var c=Math.floor(Math.random() * maplen.length);
+    var d=Math.floor(Math.random() * maplen.length);
+    var e=Math.floor(Math.random() * maplen.length);
+
+    changeColor(a,b,c,d,e)
 }
 
-function changeColor(map, num1, num2, num3, num4, num5)
+function changeColor(num1, num2, num3, num4, num5)
 {
-    document.getElementsByClassName(map)[num1].style.borderTopColor = "blue"
-    document.getElementsByClassName(map)[num2].style.borderTopColor = "purple"
-    document.getElementsByClassName(map)[num3].style.borderTopColor = "yellow"
-    document.getElementsByClassName(map)[num4].style.borderTopColor = "green"
-    document.getElementsByClassName(map)[num5].style.borderTopColor = "orange"
+
+    for (var i = 0; i < trd.length; i ++) 
+    {
+        trd[i].style.borderTopColor = "red"
+    }
+
+    document.getElementsByClassName(actualMap)[num1].style.borderTopColor = "blue"
+    document.getElementsByClassName(actualMap)[num2].style.borderTopColor = "purple"
+    document.getElementsByClassName(actualMap)[num3].style.borderTopColor = "yellow"
+    document.getElementsByClassName(actualMap)[num4].style.borderTopColor = "green"
+    document.getElementsByClassName(actualMap)[num5].style.borderTopColor = "orange"
     
 }
 
